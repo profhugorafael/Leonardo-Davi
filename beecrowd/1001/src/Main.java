3dd66455fd4562cdd8ce6e0712507bd5574f780f
hgrafa <hugorafell@gmail.com>
2024-08-31 09:47:35 -0300

    solving 1001

---
+++ beecrowd/1001/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // scanner
+        Scanner teclado = new Scanner(System.in);
+
+        // entrada
+        int a = teclado.nextInt();
+        int b = teclado.nextInt();
+
+        int soma = a + b;
+
+        // saida
+        System.out.println("X = " + soma);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
